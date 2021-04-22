ce417a0e9b0723a91f7d5b071b29660f8dc48158
MarkFerguss <81914973+MarkFerguss@users.noreply.github.com>
2021-04-22 13:31:26 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -13,33 +13,34 @@ local str2 = [[
 
 ]]
 
-local function download(path,content)
+local function download(path,rep)
 	if fs.exists(path) then fs.remove(path) end
+	local content = http.get("https://raw.githubusercontent.com/MarkFerguss/portal/main/"..rep)
 	f = fs.open(path,"w")
 	f.write(content.readAll())
 	f.close()
 end
 
 if not fs.exists("/portal/lib/f") then
-	download("/portal/lib/f",http.get("https://raw.githubusercontent.com/MarkFerguss/portal/main/lib/f.lua"))
+	download("/portal/lib/f","lib/f.lua")
 end
 if not fs.exists("/portal/lib/API") then
-	download("/portal/lib/API",http.get("https://raw.githubusercontent.com/MarkFerguss/portal/main/lib/API.lua"))
+	download("/portal/lib/API","lib/API.lua")
 end
 if not fs.exists("/portal/lib/soundAPI") then
-	download("/portal/lib/soundAPI",http.get("https://raw.githubusercontent.com/MarkFerguss/portal/main/lib/soundAPI.lua"))
+	download("/portal/lib/soundAPI","lib/soundAPI.lua")
 end
 if not fs.exists("/portal/moderator") then
-	download("/portal/moderator",http.get("https://raw.githubusercontent.com/MarkFerguss/portal/main/moderator.lua"))
+	download("/portal/moderator","moderator.lua")
 end
 if not fs.exists("/portal/setup") then
-	download("/portal/setup",http.get("https://raw.githubusercontent.com/MarkFerguss/portal/main/setup.lua"))
+	download("/portal/setup","setup.lua")
 end
 
 if not fs.exists("/portal/launch") then
 	print(str1)
 	if read() == "y" then
-		download("/portal/launch",http.get("https://raw.githubusercontent.com/MarkFerguss/portal/main/launch.lua"))
+		download("/portal/launch","launch.lua")
 	end
 	shell.run([[cd /portal]])
 	print(str2)
